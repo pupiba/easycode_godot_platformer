1de364dde870180d9e29d4b76ef3b19039cc280c
pupiba <mrfunword@gmail.com>
2025-07-29 21:26:34 +0400

    У1. Тестовый скрипт и персонаж

---
+++ character_body_2d.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+var speed = 200
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	input_vector = input_vector.normalized()
+	
+	velocity = input_vector * speed
+	move_and_slide()
